da878e14fe87b505fbdaa478da468420c507ef69
IanDs0 <ianhalominecraft@gmail.com>
2022-03-07 20:41:24 -0300

    Finalizado em .py

--- 1.py
+++ 1.py
@@ -8,9 +8,6 @@ lol['Firstblood'] = lol['firstblood'].map({1.0:'fb' , 0.0:'no_fb'})
 lol['Result'] = lol['result'].map({1:'win' , 0:'lose'})
 
 lol.loc[lol.position=='team',('playername','champion')]='team'
-# lol.loc[lol.position==0,('ban1','ban2','ban3','ban4','ban5')]='DC'
-
-# lol['Playername'] = lol['playername'].map({0:'team'})
 
 lol = lol.drop(columns=['patch','gameid','url','datacompleteness','year','date','split','playerid','teamid','opp_csat15','golddiffat15','xpdiffat15','csdiffat15','killsat15',
     'assistsat15','deathsat15','opp_killsat15','opp_assistsat15','opp_deathsat15','assistsat10','deathsat10','opp_killsat10','opp_assistsat10','opp_deathsat10','goldat15',
@@ -23,24 +20,21 @@ lol = lol.drop(columns=['patch','gameid','url','datacompleteness','year','date',
     'firstbaron','barons','opp_barons','firsttower','towers','opp_towers','firstmidtower','firsttothreetowers','minionkills','dragons','opp_dragons','infernals','mountains',
     'clouds','oceans','elders','opp_elders','firstherald','heralds','kills','deaths','assists','firstbloodkill','firstbloodassist','firstbloodvictim','firstdragon',
 
-    'firstblood','result','participantid','playoffs','game','ban1','ban2','ban3','ban4','ban5'
+    'firstblood','result','participantid','playoffs','game',
+    'ban1','ban2','ban3','ban4','ban5',
+    'league','playername'#,'position'
     ])
 
-# lol 0 lol.drop()
-# lol.head(12)
-
-lol.to_csv("./lol.csv")
-# lol = pd.read_csv("./lol.csv",header = None)
 lol.head(10)
 
-def removeAllOcurrencesOfValueInList(_list, value):
+def Remove(_list, value):
     return list(filter(lambda x: x != value, _list))
 
 lista_lol = []
 
 for index, row in lol.iterrows():
     L_lol = row.values.tolist()
-    L_lol = removeAllOcurrencesOfValueInList(L_lol,0)
+    L_lol = Remove(L_lol,0)
     lista_lol.append(L_lol)
 
 print(lista_lol[0]) 
@@ -56,12 +50,13 @@ lol.head(1)
 from mlxtend.frequent_patterns import apriori, association_rules
 
 frequent_itemsets = apriori(lol, min_support = 0.01, use_colnames = True)
-frequent_itemsets.sort_values(by=['support'], ascending = False).head(10)
+frequent_itemsets.sort_values(by=['support'], ascending = False)
 
 from mlxtend.frequent_patterns import association_rules
 
 rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.3)
-rules.sort_values(by=['lift'], ascending = False).drop(['antecedent support', 'consequent support', 'leverage', 'conviction'], axis=1)
+rules = rules.sort_values(by=['lift'], ascending = False).drop(['antecedent support', 'consequent support', 'leverage', 'conviction'], axis=1)
 
+rules.to_csv("./lol.csv")
 
-print("Olá mundo")
+rules.head(50)
